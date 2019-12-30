c6420f17949012c57cce380c52eab56bca2eb54a
Yelskyi <59335112+Yelskyi@users.noreply.github.com>
2019-12-30 17:29:19 +0200

    Add files via upload

--- app/Services/GoogleSearchParser.php
+++ app/Services/GoogleSearchParser.php
@@ -1,36 +1,47 @@
 <?php
 namespace App\Services;
 
-use Goutte\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
 class GoogleSearchParser {
 
     public function getPosition($domain, $keyword)
     {
-        $url = 'http://google.com/search?q='.$keyword.'&num=100';
-        $client = new Client();
-
-        $crawler = $client->request('GET', $url);
-
-        //Не работает
-        /*$proxylist = [
-            '122.201.112.114:80',
-            '101.50.1.2:80',
-            '138.201.72.117:80',
+        $url = 'https://google.com/search?q='.$keyword.'&num=100';
+        //Рабочий вариант, но не для гугла
+        $proxylist = [
+            '51.158.108.135:8811',
+            '167.172.35.224:8080',
+            '51.158.113.142:8811',
+            '51.158.68.68:8811',
+            '104.244.75.26:8080',
+            '51.91.212.159:3128',
+            '51.158.98.121:3128',
+            '51.158.108.135:8811',
+            '104.244.77.254:8080'
         ];
+
+
         $iter=0;
+        $elements=null;
         $error=false;
         do{
             try
             {
-                $client->setClient(new GuzzleClient([
-                    'proxy' => $proxylist[$iter],
-                ]));
-                $crawler = $client->request('GET', $url);
+                $context = stream_context_create([
+                    'http' => [
+                        'proxy' => $proxylist[$iter],
+                        'request_fulluri' => false
+                                ]
+                ]);
+                $res = file_get_contents($url, false, $context);
+
+                $crawler = new Crawler($res);
+
                 $elements = $crawler->filter('.BNeawe.UPmit.AP7Wnd')->each(function(Crawler $node){
                     return $node->text();
                 });
+                //Игнор капчи
                 if(count($elements)==0)
                 {
                     $iter++;
@@ -40,6 +51,7 @@ class GoogleSearchParser {
             }
             catch (\Exception $e)
             {
+                dump("unsuccessfully");
                 $error=true;
             }
             $iter++;
@@ -48,14 +60,7 @@ class GoogleSearchParser {
         {
             return "Here is no proxy available";
         }
-        $elements = $crawler->filter('.BNeawe.UPmit.AP7Wnd')->each(function(Crawler $node){
-            return $node->text();
-        });
-        */
 
-        $elements = $crawler->filter('.BNeawe.UPmit.AP7Wnd')->each(function(Crawler $node){
-            return $node->text();
-        });
         $position=1;
         foreach ($elements as $element)
         {
